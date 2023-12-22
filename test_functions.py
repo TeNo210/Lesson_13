317b5778eb8e43baa58625db45fc764ef311e90e
USER-PC11 <petroden297@gmal.com>
2023-12-22 17:54:02 +0300

    Код с урока 13

--- test_functions.py
+++ test_functions.py
@@ -1,3 +1,4 @@
+"""
 from functions import salary,hello_who
 def test_hello_who_max():
     assert hello_who('Max') =='Hello, Max'
@@ -9,3 +10,24 @@ def test_hello_who_max():
 def test_salary():
     assert (2, 2) != 8
     assert (3, 1) != 6
+
+
+import pytest
+from functions import salary, hello_who
+
+def test_hello_who_max():
+    assert hello_who("Max") == "Hello, Max"
+    assert hello_who("Max") != "Hello,Max"
+    assert hello_who("Max") != "Hello Max"
+    assert hello_who("Max") != "Hello,Max"
+    assert hello_who("Max") != "hello, Max"
+    assert len(hello_who("Max")) == 10
+"""
+
+def test_salary():
+    assert test_salary(1,3) != 6
+    assert  != 6
+
+
+
+
